2c6826cd180af32be25ff40de0703d70ddcbc645
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-23 22:04:26 +0200

    added Navigation for >mobile

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,88 +1,27 @@
-import { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import { Variants } from "framer-motion";
-
 import Btn from "../Btn/Btn";
-import { MotionStyledNavigation } from "./styles";
+import { StyledNavigation } from "./styles";
 
 type NavigationProps = {
   className?: string;
-  isHamburgerOpen: boolean;
-  setIsHamburgerOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const Navigation = ({
-  className,
-  isHamburgerOpen,
-  setIsHamburgerOpen,
-}: NavigationProps) => {
-  const firstLinkRef = useRef<HTMLAnchorElement>(null);
-
-  // Focus the first link in the navigation when the hamburger menu opens
-  useEffect(() => {
-    if (isHamburgerOpen) {
-      if (firstLinkRef.current) {
-        firstLinkRef.current.focus();
-      }
-    }
-  }, [isHamburgerOpen]);
-
-  const navVariants: Variants = {
-    hidden: {
-      x: "100%",
-      scaleX: 0,
-      opacity: 0,
-    },
-    visible: {
-      x: 0,
-      scaleX: 1,
-      opacity: 1,
-      transition: {
-        type: "tween",
-        duration: 0.4,
-        ease: "easeInOut",
-      },
-    },
-    exit: {
-      x: "80%",
-      scaleX: 0.5,
-      opacity: 0,
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
-
+const Navigation = ({ className }: NavigationProps) => {
   return (
-    <MotionStyledNavigation
-      className={className}
-      id="navigation"
-      variants={navVariants}
-      initial="hidden"
-      animate="visible"
-      exit="exit">
+    <StyledNavigation className={className}>
       <ul>
         <li>
-          <NavLink
-            to="/about"
-            ref={firstLinkRef}
-            onClick={() => setIsHamburgerOpen(false)}>
-            About
-          </NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
-          <NavLink to="/location" onClick={() => setIsHamburgerOpen(false)}>
-            Location
-          </NavLink>
+          <NavLink to="/location">Location</NavLink>
         </li>
         <li>
-          <NavLink to="/careers" onClick={() => setIsHamburgerOpen(false)}>
-            Careers
-          </NavLink>
+          <NavLink to="/careers">Careers</NavLink>
         </li>
       </ul>
-      <Btn onClick={() => setIsHamburgerOpen(false)}>Get Scootin</Btn>
-    </MotionStyledNavigation>
+      <Btn>Get Scootin</Btn>
+    </StyledNavigation>
   );
 };
 
--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -1,30 +1,12 @@
 import styled from "styled-components";
 import { media } from "../../utils/media";
-import convertHexToRGB from "../../utils/convertHexToRGB";
-import { motion } from "framer-motion";
 
-const StyledNavigation = styled.nav`
+export const StyledNavigation = styled.nav`
   display: flex;
   justify-content: space-between;
-
-  ${media.max("md")`
-    position: fixed;
-    inset: 4.8125rem 0 0 0;
-    flex-direction: column;
-    align-items: flex-end;
-    padding: 4rem ${({ theme }) => theme.contentPadding.xs} 1.5rem;
-    background: linear-gradient(
-      to right, 
-      ${({ theme }) => convertHexToRGB(theme.colors.darkNavy, 0.6)} 30%, 
-      ${({ theme }) => theme.colors.darkNavy} 30%
-    );
-    text-align: right;
-  `}
-
-  ${media.min("md")`
-    gap: 2.5rem;
-    align-items: center;
-  `}
+  flex: 1;
+  gap: 2.5rem;
+  align-items: center;
 
   ${media.min("lg")`
     gap: 3.5rem;
@@ -32,29 +14,14 @@ const StyledNavigation = styled.nav`
 
   ul {
     display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-    list-style: none;
-    padding: 0;
-
-    ${media.min("md")`
-      flex-direction: row;
-      /* align-items: center; */
-    `}
+    gap: 2rem;
   }
-
+  // TODO add transitions + hover effects
   li > a {
-    font-size: ${({ theme }) => theme.fontSizes.md};
     font-weight: bold;
-    color: ${({ theme }) => theme.colors.lightGrey};
     text-decoration: none;
     font-family: ${({ theme }) => theme.fontFamilies.heading};
-
-    ${media.min("md")`
-      color: ${({ theme }) => theme.colors.darkNavy};
-      font-size: ${({ theme }) => theme.fontSizes.xs};
-    `}
+    font-size: ${({ theme }) => theme.fontSizes.xs};
+    color: ${({ theme }) => theme.colors.darkNavy};
   }
 `;
-
-export const MotionStyledNavigation = motion(StyledNavigation);
