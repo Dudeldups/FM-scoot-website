76551f56da003de82d103a9e64420d2ad01d52dc
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-24 11:58:45 +0200

    added social icons as components

--- src/components/SVGs/styles.ts
+++ src/components/SVGs/styles.ts
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 import ScootLogo from "./ScootLogo";
+import SocialIcon from "./SocialIcon";
 
 export const StyledScootLogo = styled(ScootLogo)`
   path {
-    fill: ${({ theme }) => theme.colors.darkNavy};
+    header & {
+      fill: ${({ theme }) => theme.colors.darkNavy};
+    }
+
+    footer & {
+      fill: ${({ theme }) => theme.colors.white};
+    }
+  }
+`;
+
+export const StyledSocialIcon = styled(SocialIcon)`
+  width: 1.5rem;
+  height: auto;
+
+  path {
+    fill: ${({ theme }) => theme.colors.yellow};
   }
 `;
