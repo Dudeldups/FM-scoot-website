9d4fd47a0a6aa3c4ca6faea3b273be44f4d87163
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-04-15 15:49:42 +0200

    added scrolling to appStoreSection

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,26 @@
 import { NavLink } from "react-router-dom";
 import Btn from "../Btn/Btn";
 import { StyledNavigation } from "./styles";
+import { useRefStore } from "../../stores/refStore";
 
 type NavigationProps = {
   className?: string;
 };
 
 const Navigation = ({ className }: NavigationProps) => {
+  const appStoreSectionRef = useRefStore(
+    state => state.refs["appStoreSection"]
+  );
+
+  const handleGetScootinClick = () => {
+    if (appStoreSectionRef?.current) {
+      appStoreSectionRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
   return (
     <StyledNavigation className={className}>
       <ul>
@@ -20,7 +34,7 @@ const Navigation = ({ className }: NavigationProps) => {
           <NavLink to="/careers">Careers</NavLink>
         </li>
       </ul>
-      <Btn>Get Scootin</Btn>
+      <Btn onClick={() => handleGetScootinClick()}>Get Scootin</Btn>
     </StyledNavigation>
   );
 };
